51784a8e73317b82db47c8c9f581d9fbd71f2cda
Darek.Szotowicz <dariusz.szotowicz@protonmail.com>
2019-05-22 22:32:08 +0200

    project init

---
+++ script.js
@@ -0,0 +1,44 @@
+var tweetLink = "https://twitter.com/intent/tweet?text=";
+var quoteUrl = "https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1";
+var prefix = "https://cors-anywhere.herokuapp.com/";
+
+function getQuote() {
+  fetch(prefix + quoteUrl, { cache: "no-store" })
+    .then(function(resp) {
+      return resp.json();
+    })
+    .then(createTweet);
+}
+//{ cache: "no-store" } — stosujemy to, aby wyłączyć możliwość zaglądania do HTTP Cache przez przeglądarkę. Innym słowy — chcemy, aby przeglądarka za każdym razem pytała podany URL o dane.
+function createTweet(input) {
+  var data = input[0];
+
+  var dataElement = document.createElement('div');
+  dataElement.innerHTML = data.content;
+  var quoteText = dataElement.innerText.trim();
+  var quoteAuthor = data.title;
+
+  if(!quoteAuthor.length) {
+    quoteAuthor = "Unknown author";
+  }
+
+  var tweetText = "Quote of the day - " + quoteText + " Author: " + quoteAuthor;
+  if(tweetText.length > 140) {
+    getQuote();
+  } else {
+    var tweet = tweetLink + encodeURIComponent(tweetText);
+    document.querySelector('.quote').innerText = quoteText;
+    document.querySelector('.author').innerText = quoteAuthor;
+    document.querySelector('.tweet').setAttribute('href', tweet);
+
+  }
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+  getQuote();
+  console.log('getQuote');
+  document.querySelector('.trigger').addEventListener('click', function() {
+    getQuote();
+    console.log('trigger getQuote');
+  });
+});
